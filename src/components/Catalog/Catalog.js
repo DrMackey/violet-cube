8c8774e0b5a2800732a5a560ecddd0b1cc67d7eb
DrMackey <DrMackey@yandex.ru>
2024-03-11 20:49:23 +0300

    dev: create CardsGroup component

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -54,23 +54,43 @@ export default function Catalog({
         <Titlepage isTitle="Каталог" isToggleHeader={isToggleHeader} />
         <Categories />
 
-        {isLoadCards
-          ? onIsCards.map((card) => {
-              const reg = /\*|:|%|#|&|×| |-|\$/g;
-              const link = card.url.slice(8);
+        <section className="cards-group">
+          <div className="cards-group__title-container">
+            <h2 className="cards-group__title">Смотрят сегодня</h2>
+            <button className="cards-group__button">Все</button>
+          </div>
+          <ul className="cards-group__list">
+            {isLoadCards
+              ? onIsCards.map((card) => {
+                  const link = card.url.slice(8);
 
-              return (
-                <Link key={card.id} onClick={handleClick} to={`${link}`}>
-                  <h2>{card.russian}</h2>
-                  <img
-                    src={`https://shikimori.one${card.image.x96}`}
-                    alt="Изображение постера."
-                  />
-                </Link>
-              );
-            })
-          : ""}
-      </section>{" "}
+                  return (
+                    <li className="cards-group__item" key={card.id}>
+                      <Link
+                        className="card"
+                        key={card.id}
+                        onClick={handleClick}
+                        to={`${link}`}
+                      >
+                        <div className="card__image-container">
+                          <img
+                            className="card__image"
+                            src={`https://shikimori.one${card.image.x96}`}
+                            alt="Изображение постера."
+                          />
+                        </div>
+                        <div className="card__title-container">
+                          <h2 className="card__title">{card.russian}</h2>
+                          <p className="card__genre">{card.name}</p>
+                        </div>
+                      </Link>
+                    </li>
+                  );
+                })
+              : ""}
+          </ul>
+        </section>
+      </section>
       <section
         onScroll={handleScroll}
         className={`page ${isTogglePage ? "page_active" : ""}`}
