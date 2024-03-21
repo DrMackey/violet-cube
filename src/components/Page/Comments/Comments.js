1bc2f6e23c9dff1e2b34f3faab31d508accb70ad
DrMackey <DrMackey@yandex.ru>
2024-03-21 18:27:18 +0300

    dev: create styles for comments component

--- src/components/Page/Comments/Comments.js
+++ src/components/Page/Comments/Comments.js
@@ -1,255 +1,168 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Comments.css";
 
 export default function Comments() {
   return (
-    <section className="l-content-width section section--bordered">
-      <div className="section__nav">
-        <h2 className="section__headline">
-          Ratings and Reviews
-        </h2>
-
-           <a id="ember18" className="ember-view link section__nav__see-all-link" rel="nofollow" data-metrics-click="{&quot;actionType&quot;:&quot;navigate&quot;,&quot;targetType&quot;:&quot;button&quot;,&quot;targetId&quot;:&quot;SeeAllReviews&quot;}" data-we-link-to-exclude="" href="/us/app/balance-stuff/id1528333833?see-all=reviews">
-     See All
-   </a>
-
+    <section className="comments">
+      <div className="comments__title-container">
+        <h2 className="comments__title">Оценки и отзывы</h2>
+        <button className="comments__title-button">Все</button>
       </div>
 
-        <div className="we-customer-ratings lockup">
-  <div className="l-row">
-    <div className="we-customer-ratings__stats l-column small-4 medium-6 large-4">
-      <div className="we-customer-ratings__averages"><span className="we-customer-ratings__averages__display">4.5</span> out of 5</div>
-        <div className="we-customer-ratings__count small-hide medium-show">1.9K Ratings</div>
-    </div>
-    <div className=" l-column small-8 medium-6 large-4">
-      <figure className="we-star-bar-graph">
-          <div className="we-star-bar-graph__row">
-            <span className="we-star-bar-graph__stars we-star-bar-graph__stars--5"></span>
-            <div className="we-star-bar-graph__bar">
-              <div className="we-star-bar-graph__bar__foreground-bar" 
-              // style="width: 74%;"
-              ></div>
-            </div>
-          </div>
-          <div className="we-star-bar-graph__row">
-            <span className="we-star-bar-graph__stars we-star-bar-graph__stars--4"></span>
-            <div className="we-star-bar-graph__bar">
-              <div className="we-star-bar-graph__bar__foreground-bar" 
-              // style="width: 12%;"
-              ></div>
-            </div>
-          </div>
-          <div className="we-star-bar-graph__row">
-            <span className="we-star-bar-graph__stars we-star-bar-graph__stars--3"></span>
-            <div className="we-star-bar-graph__bar">
-              <div className="we-star-bar-graph__bar__foreground-bar" 
-              // style="width: 6%;"
-              ></div>
-            </div>
-          </div>
-          <div className="we-star-bar-graph__row">
-            <span className="we-star-bar-graph__stars we-star-bar-graph__stars--2"></span>
-            <div className="we-star-bar-graph__bar">
-              <div className="we-star-bar-graph__bar__foreground-bar" 
-              // style="width: 3%;"
-              ></div>
-            </div>
-          </div>
-          <div className="we-star-bar-graph__row">
-            <span className="we-star-bar-graph__stars "></span>
-            <div className="we-star-bar-graph__bar">
-              <div className="we-star-bar-graph__bar__foreground-bar" 
-              // style="width: 6%;"
-              ></div>
-            </div>
-          </div>
-      </figure>
-        <p className="we-customer-ratings__count medium-hide">1.9K Ratings</p>
-    </div>
-  </div>
-</div>
-
-
-      <div className="l-row l-row--peek">
-              
-              <div id="ember19" className="small-valign-top l-column--equal-height l-column small-4 medium-6 large-4">
-  <div>
-</div>
-
-
-  
-                <div aria-labelledby="we-customer-review-0" className="we-customer-review lockup">
-  <figure className="we-star-rating we-customer-review__rating we-star-rating--large" aria-label="4 out of 5">
-  <span className="we-star-rating-stars-outlines">
-    <span className="we-star-rating-stars we-star-rating-stars-4"></span>
-  </span>
-</figure>
-
-
-  <div className="we-customer-review__header we-customer-review__header--user">
-      <span className="we-truncate we-truncate--single-line  we-customer-review__user" dir="ltr">
-    Abby_Penguins
-</span>
-
-
-
-    <span className="we-customer-review__separator">, </span>
-
-    <time data-test-customer-review-date="" datetime="2023-09-14T22:11:15.000Z" aria-label="September 15, 2023" className="we-customer-review__date">09/15/2023</time>
-  </div>
-
-    <h3 className="we-truncate we-truncate--single-line  we-customer-review__title" dir="ltr" id="we-customer-review-0">
-    Love it but
-</h3>
-
-
-
-      <blockquote className="we-truncate we-truncate--multi-line we-truncate--interactive  we-truncate--truncated we-customer-review__body" dir="">
-      
-
-        <div data-clamp="" className="we-clamp" ismorertl="false" 
-        // style="height: 72px; mask: linear-gradient(0deg, rgba(0, 0, 0, 0) 0px, rgba(0, 0, 0, 0) 18.0001px, rgb(0, 0, 0) 18.0001px), linear-gradient(270deg, rgba(0, 0, 0, 0) 0px, rgba(0, 0, 0, 0) 32.8px, rgb(0, 0, 0) 68.8002px);"
-        >
-  <p dir="ltr" data-test-bidi="">I love this game and I love that it works offline. I found this game by searching for “zen games” when I was scrolling through and saw this game I downloaded it. Being a “zen” game, I would expect more time to stack the items. The timer just frustrates me and makes me mad when I have almost all of the things stacked and it says that I am out of time. When I am at home (or somewhere where I have internet), this isn’t a big issue, but when I am on the road (or somewhere where there is no internet) I can’t watch the ad to continue. Overall this game is very fun and I would highly recommend it.</p>
-</div>
-
-    
-
-
-      <button aria-hidden="true" tabindex="-1" className="link we-truncate__button" data-more-button="" data-metrics-click="{&quot;actionType&quot;:&quot;more&quot;,&quot;targetType&quot;:&quot;button&quot;,&quot;targetId&quot;:&quot;More&quot;}" type="button">
-        more
-      </button>
-</blockquote>
-
-
-
-</div>
-
-              
-</div>
-
-          
-    
-              <div id="ember21" className="small-valign-top l-column--equal-height l-column small-4 medium-6 large-4">
-  <div>
-</div>
-
-
-  
-                <div aria-labelledby="we-customer-review-1" className="we-customer-review lockup">
-  <figure className="we-star-rating we-customer-review__rating we-star-rating--large" aria-label="4 out of 5">
-  <span className="we-star-rating-stars-outlines">
-    <span className="we-star-rating-stars we-star-rating-stars-4"></span>
-  </span>
-</figure>
-
-
-  <div className="we-customer-review__header we-customer-review__header--user">
-      <span className="we-truncate we-truncate--single-line  we-customer-review__user" dir="ltr">
-    CaliRya
-</span>
-
-
-
-    <span className="we-customer-review__separator">, </span>
-
-    <time data-test-customer-review-date="" datetime="2024-03-17T21:12:09.000Z" aria-label="March 18, 2024" className="we-customer-review__date">03/18/2024</time>
-  </div>
-
-    <h3 className="we-truncate we-truncate--single-line  we-customer-review__title" dir="ltr" id="we-customer-review-1">
-    Timed play
-</h3>
-
-
-
-      <blockquote className="we-truncate we-truncate--multi-line we-truncate--interactive  we-truncate--truncated we-customer-review__body" dir="">
-      
-
-        <div data-clamp="" className="we-clamp" ismorertl="false" 
-        // style="height: 72px; mask: linear-gradient(0deg, rgba(0, 0, 0, 0) 0px, rgba(0, 0, 0, 0) 18.0001px, rgb(0, 0, 0) 18.0001px), linear-gradient(270deg, rgba(0, 0, 0, 0) 0px, rgba(0, 0, 0, 0) 32.8px, rgb(0, 0, 0) 68.8002px);"
-        >
-  <p dir="ltr" data-test-bidi="">This is my kind of game. Simple, relaxing, and pleasing. It’s super easy to just pick up and play four as long or little as time allows. The only thing that’s stopping me from playing it beyond “pack 1” is the timer. Why not have an option to play without a timer so I can fully relax and take my time? To me, the timer makes it stressful to play, because I have to finish before the clock runs out. If the timer was gone I would pay for the ad-free option and try the other packs/levels.</p>
-</div>
-
-    
-
-
-      <button aria-hidden="true" tabindex="-1" className="link we-truncate__button" data-more-button="" data-metrics-click="{&quot;actionType&quot;:&quot;more&quot;,&quot;targetType&quot;:&quot;button&quot;,&quot;targetId&quot;:&quot;More&quot;}" type="button">
-        more
-      </button>
-</blockquote>
-
-
-
-</div>
-
-              
-</div>
-
-          
-    
-              <div id="ember23" className="small-valign-top l-column--equal-height l-column small-4 medium-6 large-4">
-  <div>
-</div>
-
-
-  
-                <div aria-labelledby="we-customer-review-2" className="we-customer-review lockup">
-  <figure className="we-star-rating we-customer-review__rating we-star-rating--large" aria-label="4 out of 5">
-  <span className="we-star-rating-stars-outlines">
-    <span className="we-star-rating-stars we-star-rating-stars-4"></span>
-  </span></figure>
-
-
-  <div className="we-customer-review__header we-customer-review__header--user">
-      <span className="we-truncate we-truncate--single-line  we-customer-review__user" dir="ltr">
-    please help with the nicknames
-</span>
-
-
-
-    <span className="we-customer-review__separator">, </span>
-
-    <time data-test-customer-review-date="" datetime="2023-07-26T22:12:13.000Z" aria-label="July 27, 2023" className="we-customer-review__date">07/27/2023</time>
-  </div>
-
-    <h3 className="we-truncate we-truncate--single-line  we-customer-review__title" dir="ltr" id="we-customer-review-2">
-    Very fun!
-</h3>
-
-
-
-      <blockquote className="we-truncate we-truncate--multi-line we-truncate--interactive  we-truncate--truncated we-customer-review__body" dir="">
-      
-
-        <div data-clamp="" className="we-clamp" ismorertl="false" 
-        // style="height: 72px; mask: linear-gradient(0deg, rgba(0, 0, 0, 0) 0px, rgba(0, 0, 0, 0) 18.0001px, rgb(0, 0, 0) 18.0001px), linear-gradient(270deg, rgba(0, 0, 0, 0) 0px, rgba(0, 0, 0, 0) 32.8px, rgb(0, 0, 0) 68.8002px);"
-        >
-  <p dir="ltr" data-test-bidi="">I found it very fun and relaxing to play. However, right when I joined, an event popped on my screen. In my head I was like “Oh great! A cool event.” But when I started reading, a lot of the words were spelled very incorrectly. Whoever wrote it, please take your time and double check. If it was some type of glitch, then I really don’t know what to say. Other than that problem the game works out great and I really recommend it!</p>
-</div>
-
-    
-
-
-      <button aria-hidden="true" tabindex="-1" className="link we-truncate__button" data-more-button="" data-metrics-click="{&quot;actionType&quot;:&quot;more&quot;,&quot;targetType&quot;:&quot;button&quot;,&quot;targetId&quot;:&quot;More&quot;}" type="button">
-        more
-      </button>
-</blockquote>
-
-
-
-</div>
-
-              
-</div>
-
-          
-
-      </div>
-
-        <div className="l-row l-row--margin-top medium-hide">         
-
+      <div className="ratings">
+        <div className="ratings__averages">
+          <p className="ratings__averages-title">4.5</p>
+          <p className="ratings__averages-subtitle">из 5</p>
         </div>
+
+        <div className="star-bar">
+          <figure className="star-bar__graph">
+            <div className="star-bar__row">
+              <span className="star-bar__stars stars-5"></span>
+              <div className="star-bar__bar">
+                <div
+                  className="star-bar__bar_foreground-bar"
+                  style={{ width: "74%" }}
+                ></div>
+              </div>
+            </div>
+            <div className="star-bar__row">
+              <span className="star-bar__stars stars-4"></span>
+              <div className="star-bar__bar">
+                <div
+                  className="star-bar__bar_foreground-bar"
+                  style={{ width: "12%" }}
+                ></div>
+              </div>
+            </div>
+            <div className="star-bar__row">
+              <span className="star-bar__stars stars-3"></span>
+              <div className="star-bar__bar">
+                <div
+                  className="star-bar__bar_foreground-bar"
+                  style={{ width: "6%" }}
+                ></div>
+              </div>
+            </div>
+            <div className="star-bar__row">
+              <span className="star-bar__stars stars-2"></span>
+              <div className="star-bar__bar">
+                <div
+                  className="star-bar__bar_foreground-bar"
+                  style={{ width: "3%" }}
+                ></div>
+              </div>
+            </div>
+            <div className="star-bar__row">
+              <span className="star-bar__stars stars-1"></span>
+              <div className="star-bar__bar">
+                <div
+                  className="star-bar__bar_foreground-bar"
+                  style={{ width: "6%" }}
+                ></div>
+              </div>
+            </div>
+          </figure>
+          <p className="star-bar__count">32 429 оценок</p>
+        </div>
+      </div>
+
+      <div className="comments__list-container">
+        <ul className="comments__list">
+          <li className="comments__item">
+            <div className="comments__header">
+              <div className="comments__title-wrapper">
+                <h3 className="comments__title-item">Нравится, но</h3>
+                <figure className="comments__title-stars">
+                  <span className="comments__stars"></span>
+                </figure>
+              </div>
+
+              <div className="comments__time-container">
+                <time className="comments__time">1 год назад</time>
+                <span className="comments__nickname">Abby_Penguins</span>
+              </div>
+            </div>
+
+            <div className="comments__content-container">
+              <p className="comments__content">
+                I love this game and I love that it works offline. I found this
+                game by searching for “zen games” when I was scrolling through
+                and saw this game I downloaded it. Being a “zen” game, I would
+                expect more time to stack the items. The timer just frustrates
+                me and makes me mad when I have almost all of the things stacked
+                and it says that I am out of time. When I am at home (or
+                somewhere where I have internet), this isn’t a big issue, but
+                when I am on the road (or somewhere where there is no internet)
+                I can’t watch the ad to continue. Overall this game is very fun
+                and I would highly recommend it.
+              </p>
+              <button className="comments__button">еще</button>
+            </div>
+          </li>
+
+          <li className="comments__item">
+            <div className="comments__header">
+              <div className="comments__title-wrapper">
+                <h3 className="comments__title">Timed play</h3>
+                <figure className="comments__title-stars">
+                  <span className="comments__stars"></span>
+                </figure>
+              </div>
+
+              <div className="comments__time-container">
+                <time className="comments__time">1 год назад</time>
+                <span className="comments__nickname">CaliRya</span>
+              </div>
+            </div>
+
+            <div className="comments__content-container">
+              <p className="comments__content">
+                This is my kind of game. Simple, relaxing, and pleasing. It’s
+                super easy to just pick up and play four as long or little as
+                time allows. The only thing that’s stopping me from playing it
+                beyond “pack 1” is the timer. Why not have an option to play
+                without a timer so I can fully relax and take my time? To me,
+                the timer makes it stressful to play, because I have to finish
+                before the clock runs out. If the timer was gone I would pay for
+                the ad-free option and try the other packs/levels.
+              </p>
+              <button className="comments__button">еще</button>
+            </div>
+          </li>
+
+          <li className="comments__item">
+            <div className="comments__header">
+              <div className="comments__title-wrapper">
+                <h3 className="comments__title">Very fun!</h3>
+                <figure className="comments__title-stars">
+                  <span className="comments__stars"></span>
+                </figure>
+              </div>
+
+              <div className="comments__time-container">
+                <time className="comments__time">1 год назад</time>
+                <span className="comments__nickname">
+                  please help with the nicknames
+                </span>
+              </div>
+            </div>
+
+            <div className="comments__content-container">
+              <p className="comments__content">
+                I found it very fun and relaxing to play. However, right when I
+                joined, an event popped on my screen. In my head I was like “Oh
+                great! A cool event.” But when I started reading, a lot of the
+                words were spelled very incorrectly. Whoever wrote it, please
+                take your time and double check. If it was some type of glitch,
+                then I really don’t know what to say. Other than that problem
+                the game works out great and I really recommend it!
+              </p>
+              <button className="comments__button">еще</button>
+            </div>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 }
