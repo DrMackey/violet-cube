2b86d9a8c10114896415685227b71c23b4be2dfb
DrMackey <DrMackey@yandex.ru>
2024-04-07 20:36:39 +0300

    dev: add voice acting choice

--- src/components/Page/Player/Player.js
+++ src/components/Page/Player/Player.js
@@ -57,42 +57,75 @@ export default function Player({ isUrlVideos, isLoadVideo }) {
       )}
       <div className="player__nav">
         <div className="player__series">
-          <p className="player__series-title">
-            Серия
-            <span className="player__series-title-span">
-              {localCurrentSlide.index}
-              {/* {Math.trunc(scrollLeft / (clientWidth - 30) + 1.5)} */}
-            </span>
-            /<span>{isLoadVideo ? isUrlVideos[0].length : "0"}</span>
-          </p>
-          <p className="player__time-watch">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="11"
-              height="8"
-              fill="none"
-            >
-              <path
-                fill="#8D8D92"
-                d="M5.501 6.267c.591 0 1.093-.22 1.505-.662.413-.441.619-.977.619-1.607s-.207-1.165-.62-1.605a1.99 1.99 0 0 0-1.506-.66c-.591 0-1.093.22-1.505.662-.413.44-.619.977-.619 1.607s.207 1.165.62 1.605c.414.44.916.66 1.506.66Zm-.004-.774c-.39 0-.72-.145-.99-.436a1.5 1.5 0 0 1-.407-1.06c0-.416.136-.768.41-1.057.272-.289.603-.433.993-.433s.72.145.99.436a1.5 1.5 0 0 1 .407 1.06c0 .416-.136.768-.41 1.057a1.316 1.316 0 0 1-.993.433ZM5.5 8a5.365 5.365 0 0 1-3.3-1.107C1.217 6.156.483 5.191 0 4c.483-1.191 1.217-2.156 2.2-2.893A5.365 5.365 0 0 1 5.5 0c1.217 0 2.317.369 3.3 1.107.983.737 1.717 1.702 2.2 2.893-.483 1.191-1.217 2.156-2.2 2.893A5.365 5.365 0 0 1 5.5 8Zm-.002-.8c1.01 0 1.937-.291 2.783-.873A5.567 5.567 0 0 0 10.212 4a5.572 5.572 0 0 0-1.929-2.327A4.795 4.795 0 0 0 5.503.8c-1.01 0-1.938.291-2.784.873A5.657 5.657 0 0 0 .775 4a5.662 5.662 0 0 0 1.942 2.327 4.795 4.795 0 0 0 2.78.873Z"
-              />
-            </svg>
-            <span className="player__time-watch-span">13:25</span>
-          </p>
+          <div className="player__series-title-container">
+            <p className="player__series-title">
+              Серия
+              <span className="player__series-title-span">
+                {localCurrentSlide.index}
+              </span>
+              /<span>{isLoadVideo ? isUrlVideos[0].length : "0"}</span>
+            </p>
+            <p className="player__time-watch">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="11"
+                height="8"
+                fill="none"
+              >
+                <path
+                  fill="#8D8D92"
+                  d="M5.501 6.267c.591 0 1.093-.22 1.505-.662.413-.441.619-.977.619-1.607s-.207-1.165-.62-1.605a1.99 1.99 0 0 0-1.506-.66c-.591 0-1.093.22-1.505.662-.413.44-.619.977-.619 1.607s.207 1.165.62 1.605c.414.44.916.66 1.506.66Zm-.004-.774c-.39 0-.72-.145-.99-.436a1.5 1.5 0 0 1-.407-1.06c0-.416.136-.768.41-1.057.272-.289.603-.433.993-.433s.72.145.99.436a1.5 1.5 0 0 1 .407 1.06c0 .416-.136.768-.41 1.057a1.316 1.316 0 0 1-.993.433ZM5.5 8a5.365 5.365 0 0 1-3.3-1.107C1.217 6.156.483 5.191 0 4c.483-1.191 1.217-2.156 2.2-2.893A5.365 5.365 0 0 1 5.5 0c1.217 0 2.317.369 3.3 1.107.983.737 1.717 1.702 2.2 2.893-.483 1.191-1.217 2.156-2.2 2.893A5.365 5.365 0 0 1 5.5 8Zm-.002-.8c1.01 0 1.937-.291 2.783-.873A5.567 5.567 0 0 0 10.212 4a5.572 5.572 0 0 0-1.929-2.327A4.795 4.795 0 0 0 5.503.8c-1.01 0-1.938.291-2.784.873A5.657 5.657 0 0 0 .775 4a5.662 5.662 0 0 0 1.942 2.327 4.795 4.795 0 0 0 2.78.873Z"
+                />
+              </svg>
+              <span className="player__time-watch-span">13:25</span>
+            </p>
+          </div>
+          <div className="voice-acting">
+            <details className="voice-acting">
+              <summary className="voice-acting__active-button">
+                <div className="voice-acting__active-button-title-container">
+                  <p className="voice-acting__title">Animedia</p>
+                  <svg
+                    className="voice-acting__icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="10"
+                    height="8"
+                    fill="none"
+                  >
+                    <rect
+                      width="1.294"
+                      height="7.052"
+                      y="2.049"
+                      rx=".647"
+                      transform="rotate(-45 0 2.05)"
+                    />
+                    <rect
+                      width="1.294"
+                      height="7.052"
+                      x="9.068"
+                      y=".964"
+                      rx=".647"
+                      transform="rotate(43.928 9.068 .964)"
+                    />
+                  </svg>
+                </div>
+              </summary>
+              <details-menu>
+                <div className="voice-acting__list">
+                  <button className="voice-acting__item">Anilibria</button>
+                  <button className="voice-acting__item">AniDUB</button>
+                </div>
+              </details-menu>
+            </details>
+          </div>
         </div>
-        <div className="player__choice">
-          {/* <select className="player__select">
-            <option value="KODIK" selected="">
-              KODIK
-            </option>
-            <option value="Sibnet">Sibnet</option>
-          </select> */}
-          <details className="player__select">
-            <summary className="player__select__active-button">
-              <div className="player__select__active-button-title-container">
-                <p className="player__select__title">{selectedFruit}</p>
+        <div className="player-choice">
+          <details className="player-select">
+            <summary className="player-select__active-button">
+              <div className="player-select__active-button-title-container">
+                <p className="player-select__title">{selectedFruit}</p>
                 <svg
-                  className="player__select__icon"
+                  className="player-select__icon"
                   xmlns="http://www.w3.org/2000/svg"
                   width="10"
                   height="8"
@@ -115,18 +148,18 @@ export default function Player({ isUrlVideos, isLoadVideo }) {
                   />
                 </svg>
               </div>
-              <p className="player__choice_subtitle">Встроенная реклама</p>
+              <p className="player-choice__subtitle">Встроенная реклама</p>
             </summary>
             <details-menu>
-              <div className="player__select__list">
+              <div className="player-select__list">
                 <button
-                  className="player__select__item"
+                  className="player-select__item"
                   onClick={() => setSelectedFruit("KODIK")}
                 >
                   KODIK
                 </button>
                 <button
-                  className="player__select__item"
+                  className="player-select__item"
                   onClick={() => setSelectedFruit("SIBNET")}
                 >
                   SIBNET
